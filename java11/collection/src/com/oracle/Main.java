7897fe2472451dce4577ea4f004a33a7aad47fa6
reza <gholamzadreza@gmail.com>
2020-11-07 19:11:02 +0330

    convert collection to array in java 11

--- java11/collection/src/com/oracle/Main.java
+++ java11/collection/src/com/oracle/Main.java
@@ -13,6 +13,10 @@ public class Main {
 
         String[] names1 = names.toArray(new String[0]); //Before Java 11
 
+        /*
+            Before Java 11, converting a collection to array was not straightforward.
+            Java 11 makes the conversion more convenient.
+         */
         String[] names2 = names.toArray(String[]::new); //Since Java 11
     }
 }
